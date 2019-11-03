10e6860ec14fb31998d22cb28dec7bbf1fd1da84
Maddie <maddiemcampbell@gmail.com>
2019-11-03 14:23:47 -0800

    responds to request with Hello World

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// write your code here
+const http = require('http');
+
+const serverObj = http.createServer((req, res) => {
+    console.log(`receives ${req.method} request for: ${req.url}`);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Hello World');
+    res.end();
+});
+
+serverObj.listen(8080, 'localhost', null, function(){
+    console.log('Server is listening on localhost:8080');
+});
